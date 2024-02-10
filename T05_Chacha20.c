dce872a05dd212419d41966b06490cdd2b0f205a
OmarAnguiano26 <42927190+OmarAnguiano26@users.noreply.github.com>
2024-02-10 00:06:44 -0600

    Update T05_Chacha20.c

--- T05_Chacha20.c
+++ T05_Chacha20.c
@@ -8,12 +8,12 @@
 #include <stdlib.h>
 
 // Define the ChaCha20 constants
-#define ROTATE_LEFT(x, n) (((x) << (n)) | ((x) >> (32 - (n))))
-#define QUARTER_ROUND(a, b, c, d) \
-    a += b; d ^= a; d = ROTATE_LEFT(d, 16); \
-    c += d; b ^= c; b = ROTATE_LEFT(b, 12); \
-    a += b; d ^= a; d = ROTATE_LEFT(d, 8); \
-    c += d; b ^= c; b = ROTATE_LEFT(b, 7);
+#define R(x, n) (((x) << (n)) | ((x) >> (32 - (n)))) //b number of bit placed, x is number to move
+#define QR(a, b, c, d) \
+    a += b; d ^= a; d = R(d, 16); \
+    c += d; b ^= c; b = R(b, 12); \
+    a += b; d ^= a; d = R(d, 8); \
+    c += d; b ^= c; b = R(b, 7);
 
 // ChaCha20 encryption function
 void chacha20_block(uint32_t state[16]) 
@@ -21,50 +21,50 @@ void chacha20_block(uint32_t state[16])
     int i;
     uint32_t x[16];
 
-    // Copy the state to a working buffer
     memcpy(x, state, sizeof(uint32_t) * 16);
 
-    // Perform 10 rounds of ChaCha20 quarter rounds
-    for (i = 0; i < 10; ++i) {
-        QUARTER_ROUND(x[0], x[4], x[8], x[12])
-        QUARTER_ROUND(x[1], x[5], x[9], x[13])
-        QUARTER_ROUND(x[2], x[6], x[10], x[14])
-        QUARTER_ROUND(x[3], x[7], x[11], x[15])
-        QUARTER_ROUND(x[0], x[5], x[10], x[15])
-        QUARTER_ROUND(x[1], x[6], x[11], x[12])
-        QUARTER_ROUND(x[2], x[7], x[8], x[13])
-        QUARTER_ROUND(x[3], x[4], x[9], x[14])
+    for (i = 0; i < 20; ++i) 
+    {
+        QR(x[0], x[4], x[8], x[12])
+        QR(x[1], x[5], x[9], x[13])
+        QR(x[2], x[6], x[10], x[14])
+        QR(x[3], x[7], x[11], x[15])
+        QR(x[0], x[5], x[10], x[15])
+        QR(x[1], x[6], x[11], x[12])
+        QR(x[2], x[7], x[8], x[13])
+        QR(x[3], x[4], x[9], x[14])
     }
 
     // Add the original state to the ChaCha20 block
-    for (i = 0; i < 16; ++i) {
-        state[i] += x[i];
+    for (i = 0; i < 16; ++i) 
+    {
+        state[i] = x[i] + state[i];
     }
 }
 
-// ChaCha20 key setup function
+// Sets key and constants into the block
 void chacha20_keysetup(uint32_t state[16], const uint8_t *key) 
 {
-    // Constants for ChaCha20
     static const char sigma[16] = "expand 32-byte k";
     int i;
 
     // Initialize the ChaCha20 state
-    state[0] = 0x61707865; // "expa"
-    state[1] = 0x3320646e; // "nd 3"
-    state[2] = 0x79622d32; // "2-by"
-    state[3] = 0x6b206574; // "te k"
+    state[0] = 0x61707865; //  C0
+    state[1] = 0x3320646e; //  C1
+    state[2] = 0x79622d32; //  C2
+    state[3] = 0x6b206574; //  C3
 
-    // Copy the key into the state
-    for (i = 0; i < 8; ++i) {
+    // Copy the key into the block
+    for (i = 0; i < 8; ++i) 
+    {
         state[4 + i] = ((uint32_t *)key)[i];
     }
 }
 
-// ChaCha20 nonce setup function
+// Set the nonce and Ctr into the block
 void chacha20_nonce_setup(uint32_t state[16], const uint8_t *nonce) 
 {
-    // Copy the nonce into the state
+    // Copy the nonce into the block
     state[12] = 0;
     state[13] = 0;
     state[14] = ((uint32_t *)nonce)[0];
@@ -82,18 +82,21 @@ void chacha20_encrypt(const uint8_t *key, const uint8_t *nonce, const uint8_t *i
     chacha20_nonce_setup(state, nonce);
 
     // Process each block of the input
-    for (i = 0; i < size; i += 64) {
+    for (i = 0; i < size; i += 64) 
+    {
         // Generate the next ChaCha20 block
         chacha20_block(state);
 
         // XOR the input with the ChaCha20 block to produce the output
-        for (size_t j = 0; j < 64 && i + j < size; ++j) {
+        for (size_t j = 0; j < 64 && i + j < size; ++j) 
+        {
             output[i + j] = input[i + j] ^ ((uint8_t *)state)[j];
         }
 
         // Increment the block counter in the ChaCha20 state
         ++state[12];
-        if (state[12] == 0) {
+        if (state[12] == 0) 
+        {
             ++state[13];
         }
     }
@@ -105,7 +108,7 @@ int main()
     uint8_t nonce[8] = {0}; // Should be a 64-bit (8-byte) nonce
 
     // Define a message to be encrypted
-    char message[] = "Hello, ChaCha20!";
+    char message[] = "ChaCha20";
     uint8_t *encrypted_message = malloc(strlen(message));
     char *decrypted_message = malloc(strlen(message) + 1);
     // Encrypt the message
@@ -119,14 +122,12 @@ int main()
 
     // Print the encrypted and decrypted messages
     printf("Original message: %s\n", message);
-    printf("Encrypted message (hex): ");
+    printf("Encrypted message: 0x");
     for (size_t i = 0; i < strlen(message); ++i) 
     {
-        printf("%02x", encrypted_message[i]);
+        printf("%x", encrypted_message[i]);
     }
     printf("\nDecrypted message: %s\n", decrypted_message);
-
-    printf("Hola\n");
 
      // Free allocated memory
     free(encrypted_message);
